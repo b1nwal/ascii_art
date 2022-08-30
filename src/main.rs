22634ff0b22b45bad618ae6b0e98173d5e4556e5
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2022-08-30 13:54:15 -0700

    Added User-Friendly Input

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use image;
 use image::GenericImageView;
+use image::DynamicImage;
 use reqwest;
 use std::fs::File;
+use std::io::{stdin,stdout,Write};
 
 fn main() {
-    let url = "https://cdn.discordapp.com/attachments/628804839714979850/1014057928422076417/BZH3_HQ9.png";
-    get_img(url);
+    let url = get_input();
+    write_img(&url);
     let img = image::open("image.png").expect("Error Opening File.");
+    create_art(img);
+}
+
+fn create_art(img: DynamicImage) {
     let chars = [' ',',', ':', ';', 'I', 'l', '!', 'i', '>', '<', '~', '+', '_', '-', '?', ']', '[', '}', '{', '1', ')', '(', '|', '\\', '/', 't', 'f', 'j', 'r', 'x', 'n', 'u', 'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0', 'O', 'Z', 'm', 'w', 'q', 'p', 'd', 'b', 'k', 'h', 'a', 'o', '*', '#', 'M', 'W', '&', '8', '%', 'B', '@', '$'];
     let pixs = img.pixels();
     let mut brightness: usize;
@@ -19,16 +25,32 @@ fn main() {
     }
 }
 
+fn get_input() -> String {
+    let mut url = String::new();
+    println!("Please enter a URL to a low-res PNG file (128x128 is ideal)");
+    let _=stdout().flush();
+    stdin().read_line(&mut url).expect("Invalid String");
+    url.pop();
+    return url;
+}
+
 fn summate_and_normalize(x: [u8;4]) -> u8 {
     let t: u8 = ((x[0]/4 + x[1]/4 + x[2]/4) / 3) + 3 & !3;
     return t;
 }
 
-fn get_img(url: &str) -> File {
+fn get_img(url: &str) -> reqwest::blocking::Response {
+    let _res = reqwest::blocking::get(url);
+    let res = match _res {
+        Ok(a) => a,
+        Err(_) => {let url = get_input(); get_img(&url)},
+    };
+    return res;
+}
+
+fn write_img(url: &str) -> File {
+    let mut res = get_img(url);
     let mut img = File::create("image.png").expect("Error Creating File");
-    reqwest::blocking::get(url)
-        .unwrap()
-        .copy_to(&mut img)
-        .unwrap();
+    res.copy_to(&mut img).unwrap();
     return img;
 }
